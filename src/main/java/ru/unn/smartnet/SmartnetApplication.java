36bad11abe22b8f5c4342bfbd458a5b5e503839f
olta0718 <oleg.tarasov@netcracker.com>
2019-02-14 12:13:54 +0300

    spring boot start

---
+++ src/main/java/ru/unn/smartnet/SmartnetApplication.java
@@ -0,0 +1,14 @@
+package ru.unn.smartnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartnetApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/ru/unn/smartnet/SmartnetApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.unn.smartnet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartnetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
